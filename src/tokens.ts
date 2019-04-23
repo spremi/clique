6169647b8a1ed98b44d809f8d0cfadc59e522c2f
Sanjeev Premi <spremi@ymail.com>
2019-04-23 22:43:38 +0530

    feat: Define tokens used in header template
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/tokens.ts
@@ -0,0 +1,33 @@
+//
+// [clique-header]
+//
+// Sanjeev Premi (spremi@ymail.com)
+//
+// Available under terms of the BSD-3-Clause license.
+//
+// Copyright (c) 2019, Sanjeev Premi.
+//
+
+/**
+ * Defines tokens used in header template.
+ */
+export class HeaderTokens {
+	public static readonly Open = '%COPEN%';
+	public static readonly Body = '%CBODY%';
+	public static readonly Close = '%CCLOSE%';
+
+	public static readonly Author = '%AUTHOR%';
+	public static readonly Project = '%PROJECT%';
+	public static readonly License = '%LICENSE%';
+	public static readonly Copyright = '%COPY%';
+
+	public static readonly TagAuthor = '%TAGAUTHOR%';
+	public static readonly TagProject = '%TAGPROJECT%';
+	public static readonly TagLicense = '%TAGLICENSE%';
+	public static readonly TagCopy = '%TAGCOPY%';
+
+	public static readonly Space = '%SP%';
+	public static readonly InitSpace = '%ISP%';
+
+	public static readonly Year = '%YYYY%';
+}
