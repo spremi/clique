d168dfc9c689efef128d1e6a9b41f82b7b567302
Sanjeev Premi <spremi@ymail.com>
2019-04-23 22:50:52 +0530

    feat: Define various sections of header template
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ src/template.ts
@@ -0,0 +1,114 @@
+//
+// [clique-header]
+//
+// Sanjeev Premi (spremi@ymail.com)
+//
+// Available under terms of the BSD-3-Clause license.
+//
+// Copyright (c) 2019, Sanjeev Premi.
+//
+
+import { HeaderTokens, OtherTokens } from './tokens';
+
+/**
+ * Defines various sections of the header template.
+ */
+export class HeaderTemplate {
+	/**
+	 * Opens the comment block for header.
+	 */
+	public static readonly Open: string[] = [
+		`${HeaderTokens.Open}`,
+	];
+
+	/**
+	 * Closes template block for header.
+	 */
+	public static readonly Close: string[] = [
+		`${HeaderTokens.Close}`,
+	];
+
+	/**
+	 * Project name without tag.
+	 */
+	public static readonly Project: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}[${HeaderTokens.Project}]`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * Project name with tag.
+	 */
+	public static readonly ProjectTag: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.TagProject}` +
+			`${HeaderTokens.Space}${HeaderTokens.Project}`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * Author name without tag.
+	 */
+	public static readonly Author: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.Author}`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * Author name with tag.
+	 */
+	public static readonly AuthorTag: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.TagAuthor}` +
+			`${HeaderTokens.Space}${HeaderTokens.Author}`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * License name without tag.
+	 */
+	public static readonly License: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.License} License`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * License name with tag.
+	 */
+	public static readonly LicenseTag: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.TagLicense}` +
+			`${HeaderTokens.Space}${HeaderTokens.License}`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * License text without tag.
+	 */
+	public static readonly LicenseText: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}` +
+			`Available under terms of the ${HeaderTokens.License} license.`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * License name without tag - when no license is specified.
+	 */
+	public static readonly NoLicense: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${OtherTokens.Unlicensed}`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * License text without tag - when no license is specified.
+	 */
+	public static readonly NoLicenseText: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}` +
+			`${OtherTokens.Unlicensed} - No license specified.`,
+		`${HeaderTokens.Body}`,
+	];
+
+	/**
+	 * Copyright text in the header.
+	 */
+	public static readonly Copy: string[] = [
+		`${HeaderTokens.Body}${HeaderTokens.InitSpace}${HeaderTokens.Copyright}`,
+	];
+}
